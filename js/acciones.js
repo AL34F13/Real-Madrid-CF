bcbefc44bdf13d1afabc2737d7bce678c42a9978
AL34F13 <robertosev@live.com>
2015-06-25 11:07:47 -0500

    dos
    sd

--- js/acciones.js
+++ js/acciones.js
@@ -2,10 +2,10 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
-	$('#btnmenu').on ('tap',function(){
-		navigator.vibrate(2000);
-		
-	
+	$('#btnmenu').on('tap',function(){
+	navigator.vibrate(1000);	
+	alert ("unjik")
+});
 	
 	$('.ui-btn').on('tap', function (){
 		flash ($(this));
@@ -14,12 +14,26 @@ document.addEventListener("deviceready",function(){
 	
 		
 	audio = window.plugins.LowLatencyAudio;
-	
-	audio.preloadFX ('btnmenu','music/Illuminati_Confirmed.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	audio.preloadFX ('btnjugadores','music/Smoke_Weed_Everyday.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	audio.preloadFX ('btntrofeos','music/GET_NOSCOPED.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	audio.preloadFX ('btnchamp','music/DAMN_SON.mp3',function(){}, function(msg){ alert ("Error " + msg);});
-	audio.preloadFX ('sa1','music/.mp3',function(){}, function(msg){ alert ("Error " + msg);});
+	audio.preloadFX ('btnmenu','music/Illuminati_Confirmed.mp3',function(){}, function(msg){ alert ("Error " + msg);
+	});
+	audio.preloadFX ('btnjugadores','music/Smoke_Weed_Everyday.mp3',function(){}, function(msg){ alert ("Error " + msg);
+	});
+	audio.preloadFX ('btntrofeos','music/GET_NOSCOPED.mp3',function(){}, function(msg){ alert ("Error " + msg);
+	});
+	audio.preloadFX ('btnchamp','music/DAMN_SON.mp3',function(){}, function(msg){ alert ("Error " + msg);
+	});
+	audio.preloadFX ('btnmas','music/.mp3',function(){}, function(msg){ alert ("Error " + msg);	    });
+
+
+
+
+	function quien (q)
+ {
+	 audio.play(q);
+	 return q.substring(1);
+ }
+
+	});
+
 });
-});
-});
+
